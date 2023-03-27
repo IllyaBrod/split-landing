aac17c6c58ae82291c4830d2b6df6ae047f380a5
Illya Brodovskyy <491391@student.saxion.nl>
2023-03-27 17:10:43 +0200

    fix: final fix

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -5,40 +5,37 @@ import BackgroundImage from "../../public/img/bg-image.png";
 
 const Showcase = ({id}: {id?: string}) => {
   return (
-    <section className="h-screen w-full lg:flex">
 
-      <div className="h-full flex items-center">
-        <div className="text-center lg:text-left p-5 lg:px-20 lg:basis-1/2">
-          <p className=" text-9xl md:text-huge text-primary leading-none font-black">split</p>
+    <div className="h-full flex items-center max-w-[1920px]">
+      <div className="text-center lg:text-left p-5 lg:px-20 lg:basis-1/2">
+        <p className=" text-9xl md:text-huge text-primary leading-none font-black">split</p>
 
-          <h1 className="mt-16 mb-9 text-showcase">
+        <h1 className="mt-16 mb-9 text-showcase">
               Empower your business
-          </h1>
-          <p className="text-2xl text-secondary">
+        </h1>
+        <p className="text-2xl text-secondary">
               We create an application to empower taxi companies to be
               able to compete in the industry by delivering improved UX
               and higher customer satisfaction.
-          </p>
+        </p>
 
-          <LinkButton id="contact" text="Contact Us" />
-        </div>
-
-        <div className="hidden lg:block lg:w-1/2 lg:basis-1/2 lg:self-start">
-          <Image
-            src={BackgroundImage}
-            alt={"Background image"}
-            style={{
-              objectFit: "cover",
-              height: "100vh",
-            }}
-            quality={80}
-          />
-        </div>
-
+        <LinkButton id="contact" text="Contact Us" />
       </div>
 
+      <div className="hidden lg:block lg:w-1/2 lg:basis-1/2 lg:self-start">
+        <Image
+          src={BackgroundImage}
+          alt={"Background image"}
+          style={{
+            objectFit: "cover",
+            height: "100vh",
+          }}
+          quality={80}
+        />
+      </div>
 
-    </section>
+    </div>
+
 
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -215,7 +215,7 @@ const Home: FC = () => {
             <form action="https://formsubmit.co/23c438f4bfa2a1fee45c218ba4e699d1" method="POST">
               <input type="text" name="_honey" className="hidden" />
               <input type="hidden" name="_captcha" value="false" />
-              <input type="hidden" name="_next" value="http://localhost:3000" />
+              <input type="hidden" name="_next" value="http://https://split-landing-phi.vercel.app" />
 
               <div className="grid lg:grid-cols-2 gap-y-11">
                 <div>
