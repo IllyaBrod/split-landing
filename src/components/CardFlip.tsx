87732be61daa86f3f76d35217e60ebef4a972ef3
Illya Brodovskyy <491391@student.saxion.nl>
2023-04-04 17:15:37 +0200

    fix: card application not turning

--- src/components/CardFlip.tsx
+++ src/components/CardFlip.tsx
@@ -6,16 +6,18 @@ const CardFlip = ({imageData, title, description, background, backDescription}:
   {imageData: StaticImageData, title: string, description: string,
     background: string, backDescription: string}) => {
   return (
-    <div className={styles.container}>
-      <div className={`${styles.front} flex flex-col items-center
-       rounded-4xl text-center px-7 pb-7 pt-3  ${background}`}>
-        <Image src={imageData} height={270} alt="conversion image" />
-        <h2 className="mb-6 mt-2">{title}</h2>
-        <p className="text-xl">{description}</p>
-      </div>
-      <div className={`${styles.back}`}>
-        <div className="flex h-full flex-col items-center justify-center bg-showcase/95 w-full px-7 rounded-4xl">
-          <p className="text-xl text-white">{backDescription}</p>
+    <div className="max-w-[400px] md:w-[400px] h-[450px]">
+      <div className={styles.container}>
+        <div className={`${styles.front} flex flex-col items-center
+         rounded-4xl text-center px-7 pb-7 pt-3  ${background}`}>
+          <Image src={imageData} height={270} alt="conversion image" />
+          <h2 className="mb-6 mt-2">{title}</h2>
+          <p className="text-xl">{description}</p>
+        </div>
+        <div className={`${styles.back}`}>
+          <div className="flex h-full flex-col items-center justify-center bg-showcase/95 w-full px-7">
+            <p className="text-xl text-white">{backDescription}</p>
+          </div>
         </div>
       </div>
     </div>
